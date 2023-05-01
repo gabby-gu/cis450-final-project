b447c82f24637a93fb52dbee76a0d441e1ce4c8d
panchel <panchel@seas.upenn.edu>
2023-05-01 13:05:46 -0400

    fixed basic homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,76 +3,57 @@ import { Container, Divider, Link } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 import LazyTable from '../components/LazyTable';
-import SongCard from '../components/SongCard';
 const config = require('../config.json');
 
 export default function HomePage() {
-  // We use the setState hook to persist information across renders (such as the result of our API calls)
-  const [songOfTheDay, setSongOfTheDay] = useState({});
-  // TODO (TASK 13): add a state variable to store the app author (default to '')
-  const [author, setAuthor] = useState('');
+  const [movie, setmovie] = useState([]);
+  const [sortRelease, setSortRelease] = useState([]);
+  const [threeUsersThreeGenres, setThreeUsersThreeGenres] = useState([]);
 
-  const [selectedSongId, setSelectedSongId] = useState(null);
-
-
-
-  // The useEffect hook by default runs the provided callback after every render
-  // The second (optional) argument, [], is the dependency array which signals
-  // to the hook to only run the provided callback if the value of the dependency array
-  // changes from the previous render. In this case, an empty array means the callback
-  // will only run on the very first render.
   useEffect(() => {
-
-    // TODO (TASK 14): add a fetch call to get the app author (name not pennkey) and store it in the state variable
     fetch(`http://${config.server_host}:${config.server_port}/home`)
-    .then(res => res.text())
-    .then(resText => setAuthor(resText));
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.default);
+        setmovie(data.default);
+        setSortRelease(data.sortRelease);
+        setThreeUsersThreeGenres(data.threeUsersThreeGenres);
+      });
   }, []);
 
-  // Here, we define the columns of the "Top Songs" table. The songColumns variable is an array (in order)
-  // of objects with each object representing a column. Each object has a "field" property representing
-  // what data field to display from the raw data, "headerName" property representing the column label,
-  // and an optional renderCell property which given a row returns a custom JSX element to display in the cell.
-  const songColumns = [
-    {
-      field: 'title',
-      headerName: 'Movie Title',
-      renderCell: (row) => <Link onClick={() => setSelectedSongId(row.song_id)}>{row.title}</Link> // A Link component is used just for formatting purposes
-    },
-    // {
-    //   field: 'album',
-    //   headerName: 'Album',
-    //   renderCell: (row) => <NavLink to={`/albums/${row.album_id}`}>{row.album}</NavLink> // A NavLink component is used to create a link to the album page
-    // },
-    // {
-    //   field: 'plays',
-    //   headerName: 'Plays',
-    //   renderCell: (row) => <p>{row.plays}</p>
-    // },
-  ];
-
-  // TODO (TASK 15): define the columns for the top albums (schema is Album Title, Plays), where Album Title is a link to the album page
-  // Hint: this should be very similar to songColumns defined above, but has 2 columns instead of 3
-  const albumColumns = [
-    {
-      field: 'title',
-      headerName: 'Album Title',
-      renderCell: (row) => <NavLink to={`/albums/${row.album_id}`}>{row.title}</NavLink> // A NavLink component is used to create a link to the album page
-    },
-    {
-      field: 'plays',
-      headerName: 'Plays'
-    },
-
-  ]
-
   return (
- 
-
-    <Container maxWidth={false} style={{ width: '100%', backgroundImage: `url('https://www.domusweb.it/content/dam/domusweb/it/art/2019/11/17/parasite-il-film-dellanno-usa-larchitettura-per-parlare-a-tutti/parasite-2-movie-domus_risultato.jpg.foto.rbig.jpg')` }}>
-  {/* your other code */}
-</Container>
-
-
+    <Container maxWidth="lg">
+      <h1>Welcome to CineScore</h1>
+      <h2> Movies to Try: </h2>
+      <ul>
+        {movie.map((item) => (
+          <li key={item.movie_id}>
+            <NavLink to={`/movie/m${item.movie_id}`}>
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <h2>Sorted by Release Date: </h2>
+      <ul>
+        {sortRelease.map((item) => (
+          <li key={item.movie_id}>
+            <NavLink to={`/movie/l${item.movie_id}`}>
+              {item.title} - {item.release_date} - {item.avg}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <h2>Three Users Three Genres: </h2>
+      <ul>
+        {threeUsersThreeGenres.map((item) => (
+          <li key={item.user_id}>
+            <NavLink to={`/user/${item.user_id}`}>
+            {item.user_id} - {item.tag} - {item.avg_rating_of_genre}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </Container>
   );
-};
+}
--- client/src/pages/MovieInfoPage.js
+++ client/src/pages/MovieInfoPage.js
@@ -6,66 +6,66 @@ import { formatReleaseDate } from '../helpers/formatter';
 const config = require('../config.json');
 
 export default function MovieInfoPage() {
-  const { movie_id } = useParams();
+  const { movie_id } = useParams();
 
-  const [movieData, setmovieData] = useState({});
-  const [posterUrl, setPosterUrl] = useState('');
-  const [userList, setUserList] = useState([]);
+  const [movieData, setmovieData] = useState({});
+  const [posterUrl, setPosterUrl] = useState('');
+  const [userList, setUserList] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://${config.server_host}:${config.server_port}/movie/${movie_id}`)
-      .then(res => res.json())
-      .then(resJson => {
-        console.log(resJson);
-        setmovieData(resJson.movieInfoResults[0]);
-        setUserList(resJson.userList);
-        const imdb_id = resJson.movieInfoResults[0].imdb_id;
-        const api_key = 'afeb2e4f';
-        const url = `http://www.omdbapi.com/?apikey=${api_key}&i=${imdb_id}`;
-        fetch(url)
-          .then(response => response.json())
-          .then(data => {
-            const posterUrl = data.Poster;
-            console.log(posterUrl); // this should output the poster URL
-            setPosterUrl(posterUrl);
-          })
-          .catch(error => console.error(error));
-      })
-      .catch(error => console.error(error));
-  }, [movie_id]);
+  useEffect(() => {
+    fetch(`http://${config.server_host}:${config.server_port}/movie/${movie_id}`)
+      .then(res => res.json())
+      .then(resJson => {
+        console.log(resJson);
+        setmovieData(resJson.movieInfoResults[0]);
+        setUserList(resJson.userList);
+        const imdb_id = resJson.movieInfoResults[0].imdb_id;
+        const api_key = 'afeb2e4f';
+        const url = `http://www.omdbapi.com/?apikey=${api_key}&i=${imdb_id}`;
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            const posterUrl = data.Poster;
+            console.log(posterUrl);
+            setPosterUrl(posterUrl);
+          })
+          .catch(error => console.error(error));
+      })
+      .catch(error => console.error(error));
+  }, [movie_id]);
 
-  return (
-    <Container style={{ marginTop: '70px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ display: 'flex' }}>
-        {posterUrl && <img src={posterUrl} style={{ width: '40%', marginRight: '20px' }} />}
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-          <h1 style={{ fontFamily: 'Arial', fontSize: '40px', margin: 0, fontWeight: 'bold' }}>{movieData.title}</h1>
-          <p style={{ fontFamily: 'Arial', fontSize: '22px', margin: 0 }}>
-            <span style={{ fontWeight: 'bold', color: 'blue' }}>Release Year: </span>
-            {formatReleaseDate(movieData.release_date)}
-          </p>
-          <p style={{ fontFamily: 'Arial', fontSize: '22px', margin: 0 }}>
-            <span style={{ fontWeight: 'bold', color: 'blue' }}>Average Rating: </span>
-            {movieData.avg_rating}
-          </p>
-          <p style={{ fontFamily: 'Arial', fontSize: '22px', margin: 0 }}>
-            <span style={{ fontWeight: 'bold', color: 'blue' }}>Synopsis: </span>
-            {movieData.overview}
-          </p>
-        </div>
-      </div>
-      {userList.length > 0 && (
-        <div style={{ marginTop: '20px' }}>
-          <h2 style={{ fontFamily: 'Arial', fontSize: '28px', margin: 0 }}>Users:</h2>
-          <ul style={{ fontFamily: 'Arial', fontSize: '22px', margin: '10px 0 0 0', padding: 0 }}>
-            {userList.map(user => (
-              <li key={user.user_id}>
-                <a href={`/user/${user.user_id}`}>{user.user_id}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </Container>
-  );
+  return (
+    <Container style={{ marginTop: '70px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ display: 'flex' }}>
+        {posterUrl && <img src={posterUrl} style={{ width: '40%', marginRight: '20px' }} />}
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <h1 style={{ fontFamily: 'Arial', fontSize: '40px', margin: 0, fontWeight: 'bold' }}>{movieData.title}</h1>
+          <p style={{ fontFamily: 'Arial', fontSize: '22px', margin: 0 }}>
+            <span style={{ fontWeight: 'bold', color: 'blue' }}>Release Year: </span>
+            {formatReleaseDate(movieData.release_date)}
+          </p>
+          <p style={{ fontFamily: 'Arial', fontSize: '22px', margin: 0 }}>
+            <span style={{ fontWeight: 'bold', color: 'blue' }}>Average Rating: </span>
+            {movieData.avg_rating}
+          </p>
+          <p style={{ fontFamily: 'Arial', fontSize: '22px', margin: 0 }}>
+            <span style={{ fontWeight: 'bold', color: 'blue' }}>Synopsis: </span>
+            {movieData.overview}
+          </p>
+        </div>
+      </div>
+      {userList.length > 0 && (
+        <div style={{ marginTop: '20px', marginBottom: '50px' }}>
+          <h2 style={{ fontFamily: 'Arial', fontSize: '28px', margin: 20, color: 'blue'}}>Users:</h2>
+          <ul style={{ fontFamily: 'Arial', fontSize: '22px', margin: '10px 0 0 0', padding: 0 }}>
+            {userList.map(user => (
+              <li key={user.user_id}>
+                <a href={`/user/${user.user_id}`}>{user.user_id}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </Container>
+  );
 }
